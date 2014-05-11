f30ad3d54255e489c798ba4d47ce0ca52eb222dc
Haralan Dobrev <hkdobrev@gmail.com>
2014-05-11 18:14:50 +0300

    Add PrintHometown class to print my hometown

---
+++ 1-java-basics/print-hometown/PrintHometown.java
@@ -0,0 +1,8 @@
+class PrintHometown {
+
+    public static final String HOMETOWN = "Pazardzhik";
+
+    public static void main(String[] args) {
+        System.out.println("My hometown is " + PrintHometown.HOMETOWN);
+    }
+}
